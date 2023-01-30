f398f93e193314024fb287ff0710ef58f2ecdbef
Youngseo Yook <32454317+display1180@users.noreply.github.com>
2023-01-30 09:44:18 +0900

    fourth

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
 
 
+
+const loadingScreen = document.querySelector('.loading-screen');
+gsap.registerPlugin(ScrollTrigger);
+
+function pageTransitionIn() {
+    return gsap.to(loadingScreen, 0.5, {scaleY:1, transformOrigin:'top left'})
+}
+function pageTransitionOut() {
+    return gsap.timeline({delay:1}).add('start').to(loadingScreen, 0.5, {scaleY:0,transformOrigin:'bottom left', delay:0.25},'start').call(contentAnimation,[container] , 'start');
+}
+
+function contentAnimation(container) {
+    return gsap.timeline().from('h1',{opacity:0, translateY:10, duration:0.5})
+}
 barba.init({
     transitions: [{
         name: 'first-barba',
-        leave(data) {
-            return gsap.to(data.current.container,1,  {
-                opacity:0
-            });
+        async leave(data) {
+            await pageTransitionIn();
+            data.current.container.remove();
         },
-        enter(data) {
-            return gsap.from(data.next.container,1,  {
-                opacity:0,
-            })
+        async enter(data) {
+           await pageTransitionOut(data.next.container);
         }
     }]
 });
+
+const tl = gsap.timeline().from(".sect2 p", {
+    opacity:0,
+    scrollTrigger: {
+        markers:true,
+        trigger:'.sect2 p',
+        scrub:true,
+        start: 'top center',
+        end: 'bottom center'
+    }
+})
+.to('.sect2 p', {
+    opacity:1,
+    scrollTrigger: {
+        markers:{
+            startColor:'blue',
+            endColor:'yellow'
+        },
+        trigger:'.sect2 p',
+        scrub:true,
+        start:'top+=400 center',
+        end: 'top+=500 center'
+    }
+})
